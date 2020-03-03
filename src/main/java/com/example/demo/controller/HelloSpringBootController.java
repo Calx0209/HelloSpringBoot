453389cfefeacc14c94fcdb921335276b3919ccd
Calx <robuk@bokuno.com>
2020-03-03 16:15:50 +0900

    add JavaDoc

--- src/main/java/com/example/demo/controller/HelloSpringBootController.java
+++ src/main/java/com/example/demo/controller/HelloSpringBootController.java
@@ -1,12 +1,19 @@
-package com.example.demo;
+package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
+/**
+ * HelloSpringBootController.
+ */
 @Controller
 public class HelloSpringBootController {
+    /**
+     * Returns index page.
+     * 
+     * @param model is model class
+     */
     @RequestMapping("/")
     public String index(Model model) {
         model.addAttribute("message", "Hello Springboot");
